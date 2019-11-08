96e9c99f53b5d15edb2d65d84e9d33e0ddb82c03
Jon Anderson <jon@hitiq.co>
2019-11-08 15:35:22 +1100

    Restructured includes

--- inc/hw_init.h
+++ inc/hw_init.h
@@ -1,7 +1,10 @@
 #ifndef __HW_INIT_H__
 #define __HW_INIT_H__
 
-#include "main.h"
+#include "hardware.h"
+#include "stm32f103xb.h"
+#include "gpio.h"
+#include "dma.h"
 
 void GPIO_Init(void);
 void SystemClock_Config(void);
